425c0d557c8d270a37724f8f4f9c39ba81ef807e
jaforjaforjafor <jaforiqbalbd1996@gmail.com>
2021-08-21 17:36:30 +0600

    before total price

--- js/app.js
+++ js/app.js
@@ -1,18 +1,69 @@
-// document.getElementById('memory-access').addEventListener('click',function(){
-//    const clickMemoryField = document.getElementById('memory-cost');
-//     clickMemoryField.innerText=clickMemoryField;
-
-
-
-const value =20;
-
-document.getElementById("memory-access").addEventListener("click", displayvalue);
-const mouseClick =document.getElementById('memory-access');
-const clickMemoryField = document.getElementById('memory-cost');
-
-function displayvalue() {
-  document.getElementById("memory-cost").innerHTML = value;
+//for bestPrice
+let bestPrice = 1219
+//for memory 
+// 8 gb
+const addingEightGb =document.getElementById('memory-access8');
+addingEightGb.addEventListener('click',function(){
+    calculateEightGbMemory();
+});
+function calculateEightGbMemory(){
+    const memoryExtraCost =0;
+    document.getElementById('memory-cost').innerText=memoryExtraCost;
+}
+//for 16 gb
+const addSixteenGb = document.getElementById('memory-access16');
+addSixteenGb.addEventListener('click',function(){
+    calculateSixteenGbMemory();
+});
+function calculateSixteenGbMemory(){
+    const memoryExtraCost =180;
+    document.getElementById('memory-cost').innerText=memoryExtraCost;
+}
+//for Storage
+//for 256
+const addingTwoFiftySixStorage =document.getElementById('storage-access-256');
+addingTwoFiftySixStorage.addEventListener('click',function(){
+    calculateTwoFiftySixStorage();
+});
+function calculateTwoFiftySixStorage(){
+  const  storageExtraCost =0;
+  document.getElementById('storage-cost').innerText=storageExtraCost;
+}
+//for 512
+const addingFiveTwelveStorage =document.getElementById('storage-access-512');
+addingFiveTwelveStorage.addEventListener('click',function(){
+    calculateFiveTwelveStorage();
+});
+function calculateFiveTwelveStorage(){
+  const  storageExtraCost =100;
+  document.getElementById('storage-cost').innerText=storageExtraCost;
+}
+//for 1 tera-byte
+const addingTerabyteStorage =document.getElementById('storage-access-1tb');
+addingTerabyteStorage.addEventListener('click',function(){
+    calculateTerabyteStorage();
+});
+function calculateTerabyteStorage(){
+  const  storageExtraCost =180;
+  document.getElementById('storage-cost').innerText=storageExtraCost;
+}
+//delivery process
+//regular process
+const deliveryProcess =document.getElementById('regular-delivery');
+deliveryProcess.addEventListener('click',function(){
+    calculateRegularDelivery();
+});
+function calculateRegularDelivery(){
+  const  regularDeliveryCost =0;
+  document.getElementById('delivery-cost').innerText=regularDeliveryCost;
+}
+//quick process
+const quickProcess =document.getElementById('quick-delivery');
+quickProcess.addEventListener('click',function(){
+    calculateQuickDelivery();
+});
+function calculateQuickDelivery(){
+  const  quickDeliveryCost =20;
+  document.getElementById('delivery-cost').innerText = quickDeliveryCost;
   
 }
-   
-// })
