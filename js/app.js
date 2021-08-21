9983845b685e7b1b333af89724b862dff37c27f2
jaforjaforjafor <jaforiqbalbd1996@gmail.com>
2021-08-21 22:09:51 +0600

    assignment-5

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 //for bestPrice
-let bestPrice = 1219
+// let bestPrice = 1219;
 //for memory 
 // 8 gb
 const addingEightGb =document.getElementById('memory-access8');
 addingEightGb.addEventListener('click',function(){
     calculateEightGbMemory();
+      calculateTotal();
 });
 function calculateEightGbMemory(){
     const memoryExtraCost =0;
@@ -12,18 +13,24 @@ function calculateEightGbMemory(){
 }
 //for 16 gb
 const addSixteenGb = document.getElementById('memory-access16');
+const newTotalOfGb =addSixteenGb.value;
 addSixteenGb.addEventListener('click',function(){
     calculateSixteenGbMemory();
+     calculateTotal();
 });
 function calculateSixteenGbMemory(){
     const memoryExtraCost =180;
+    const newTotal = document.getElementById('memory-cost').innerText
     document.getElementById('memory-cost').innerText=memoryExtraCost;
+    
+    
 }
 //for Storage
 //for 256
 const addingTwoFiftySixStorage =document.getElementById('storage-access-256');
 addingTwoFiftySixStorage.addEventListener('click',function(){
     calculateTwoFiftySixStorage();
+     calculateTotal();
 });
 function calculateTwoFiftySixStorage(){
   const  storageExtraCost =0;
@@ -33,6 +40,7 @@ function calculateTwoFiftySixStorage(){
 const addingFiveTwelveStorage =document.getElementById('storage-access-512');
 addingFiveTwelveStorage.addEventListener('click',function(){
     calculateFiveTwelveStorage();
+    calculateTotal();
 });
 function calculateFiveTwelveStorage(){
   const  storageExtraCost =100;
@@ -42,6 +50,7 @@ function calculateFiveTwelveStorage(){
 const addingTerabyteStorage =document.getElementById('storage-access-1tb');
 addingTerabyteStorage.addEventListener('click',function(){
     calculateTerabyteStorage();
+    calculateTotal();
 });
 function calculateTerabyteStorage(){
   const  storageExtraCost =180;
@@ -52,6 +61,7 @@ function calculateTerabyteStorage(){
 const deliveryProcess =document.getElementById('regular-delivery');
 deliveryProcess.addEventListener('click',function(){
     calculateRegularDelivery();
+    calculateTotal();
 });
 function calculateRegularDelivery(){
   const  regularDeliveryCost =0;
@@ -61,9 +71,34 @@ function calculateRegularDelivery(){
 const quickProcess =document.getElementById('quick-delivery');
 quickProcess.addEventListener('click',function(){
     calculateQuickDelivery();
+    calculateTotal();
 });
 function calculateQuickDelivery(){
   const  quickDeliveryCost =20;
   document.getElementById('delivery-cost').innerText = quickDeliveryCost;
   
 }
+//for promo code
+document.getElementById('code-apply').addEventListener('click', function(){
+    
+const codeField =document.getElementById('promo-code');
+const promoField =codeField.value;
+if(promoField == 'stevekaku'){
+
+    window.location.href = 'promo.html';
+
+}
+});
+
+
+
+ function calculateTotal(){
+     const memoryCostNumber =parseInt(document.getElementById('memory-cost').innerText);
+   const storageCostNumber =parseInt(document.getElementById('storage-cost').innerText);
+    const deliveryCostNumber =parseInt(document.getElementById('delivery-cost').innerText);
+    const bestPriceNumber =parseInt(document.getElementById('default-price').innerText);
+   let totalPrice= document.getElementById('total-Price').innerText = memoryCostNumber+storageCostNumber+deliveryCostNumber+bestPriceNumber;
+   console.log(totalPrice);
+};
+
+ 
